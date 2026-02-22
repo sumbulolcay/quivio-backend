44dd464aa9f65c3e4fb258d6d89f240c6205abf3
sumbulolcay <sumbulolcay@gmail.com>
2026-02-22 15:46:05 +0300

    Add debug logging for reCAPTCHA verification result.

--- src/middlewares/recaptcha.js
+++ src/middlewares/recaptcha.js
@@ -48,6 +48,7 @@ function recaptchaMiddleware(req, res, next) {
     return res.status(400).json({ code: 'recaptcha_failed', message: 'reCAPTCHA token gerekli' });
   }
   verifyRecaptcha(token).then((result) => {
+    console.log("recaptcha result", result);
     if (!result.success) {
       return res.status(429).json({ code: 'recaptcha_failed', message: 'reCAPTCHA doğrulaması başarısız' });
     }
